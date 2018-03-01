50ae39e66a7ca974250a0cd1f1b70872117b5649
Jairo <jairo_1193@hotmail.com>
2018-03-01 16:01:54 -0500

    vistas y crear texto

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,30 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
+from django.shortcuts import redirect
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+from .forms import PostForm
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+def post_list(request):
+        posts = Post.objects.filter(title__contains ='galleta') 
+        return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_detail (request, pk):
+		Post.objects.get(pk=pk)
+		post = get_object_or_404(Post, pk=pk)
+		return render(request, 'blog/post_detail.html', {'post': post})
+def post_new(request):
+	if request.method == "POST":
+		form = PostForm(request.POST)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author = request.user
+			post.published_date = timezone.now()
+			post.save()
+			return redirect('post_detail', pk=post.pk)
+	else:
+		form = PostForm()
+	return render(request, 'blog/post_edit.html', {'form': form})
+
